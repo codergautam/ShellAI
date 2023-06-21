f7e54b892677051dd0b2d0e2126c94f9ee8eeb32
codergautam <66850133+codergautam@users.noreply.github.com>
2023-06-21 02:09:41 +0000

    feat: stop task when it's done

--- index.js
+++ index.js
@@ -2,18 +2,16 @@ import { Configuration, OpenAIApi } from "openai";
 import { exec } from 'child_process';
 
 const openai = new OpenAIApi(new Configuration({
-    apiKey: process.env.OPENAI_API_KEY // <-- YOUR KEY
+  apiKey: process.env.OPENAI_API_KEY // <-- YOUR KEY
 }));
 
 async function runCommand(cmd) {
-  console.log("Running command:", cmd)
+  console.log("Running command:", cmd);
   return new Promise((resolve, reject) => {
     exec(cmd, (error, stdout, stderr) => {
       if (error) {
         resolve(error.toString());
       }
-
-
       resolve(stdout);
     });
   });
@@ -21,12 +19,12 @@ async function runCommand(cmd) {
 
 async function chatWithAI() {
   let gptResponse = null;
-  let history= [
+  let history = [
     {
-          role: "user",
-          content: "Create a new folder called files and inside it, make a text file called README.md. that says 'made by AI'"
-        }
-    ]
+      role: "user",
+      content: "what is the weather in rochester mn. Run the 'end' functions when the weather has been found or cannot be found."
+    }
+  ];
 
   while (true) {
     if (gptResponse && gptResponse.data.choices && gptResponse.data.choices.length > 0) {
@@ -34,49 +32,60 @@ async function chatWithAI() {
       if (functionCall && functionCall.name === "run_cli_command") {
         const command = JSON.parse(functionCall.arguments).command;
         const output = await runCommand(command);
-          history.push({
-            role: "function",
-            name: "run_cli_command",
-            content: output ?? "Executed command successfully"
-          });
+        history.push({
+          role: "function",
+          name: "run_cli_command",
+          content: output ?? "Executed command successfully"
+        });
+      } else if (functionCall && functionCall.name === "end") {
+        console.log("Task completed.");
+        break;
       }
     }
-try {
-  console.log(history)
-    gptResponse = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo-0613",
-      messages: history,
-      functions: [
-        {
-          name: "run_cli_command",
-          description: "Run a cli command",
-          parameters: {
-            type: "object",
-            properties: {
-              command: {
-                type: "string",
-                description: "The command to run"
+
+    try {
+      gptResponse = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo-0613",
+        messages: history,
+        functions: [
+          {
+            name: "run_cli_command",
+            description: "Run a cli command",
+            parameters: {
+              type: "object",
+              properties: {
+                command: {
+                  type: "string",
+                  description: "The command to run"
+                },
               },
+              required: ["command"]
+            }
+          },
+          {
+            name: "end",
+            description: "Mark the task as complete and end the program.",
+            parameters: {
+              type: "object",
+              properties: {}
             },
-            required: ["command"]
+            required: []
           }
-        }
-      ],
-      function_call: history[history.length - 1].role == "function" ? "none" : {name: "run_cli_command"} ,
-    });
-  if(gptResponse.data.choices[0].message.content) {
-    history.push({
-      role: gptResponse.data.choices[0].message.role,
-      content: gptResponse.data.choices[0].message.content
-    });
-    console.log(gptResponse.data.choices[0].message.content);
-  }
-    
-    // console.log( history[history.length - 1].role);
-  
-  } catch (error) {
-    throw new Error("error");
-}
+        ],
+        function_call: history[history.length - 1].role == "function" ? "none" : "auto",
+      });
+
+      if (gptResponse.data.choices[0].message.content) {
+        history.push({
+          role: gptResponse.data.choices[0].message.role,
+          content: gptResponse.data.choices[0].message.content
+        });
+        console.log(gptResponse.data.choices[0].message.content);
+      }
+    } catch (error) {
+      console.log(error.message);
+      throw new Error("error");
+    }
   }
 }
 
